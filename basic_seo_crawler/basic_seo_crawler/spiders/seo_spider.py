2fa9c713b6a3b2a83fb750b43235b8fd61a866fb
George <georgethomaschristie@gmail.com>
2020-01-31 18:07:03 +0000

    First commit. Basic crawler

---
+++ basic_seo_crawler/basic_seo_crawler/spiders/seo_spider.py
@@ -0,0 +1,27 @@
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from urllib.parse import urlsplit
+
+
+class SEOSpider(CrawlSpider):
+	name = 'seospider'
+	allowed_domains = ['georgechristie.dev']
+	start_urls = ['https://georgechristie.dev']
+
+	rules = (
+		Rule(LinkExtractor(), callback='parse_item'),
+	)
+
+	def parse_item(self, response):
+		return {
+			'url': response.url,
+			'path': urlsplit(response.url).path,
+			'content': response.headers['Content-Type'],
+			'status_code': response.status,
+			'title': response.xpath('//title/text()').get(),
+			'meta_description': response.xpath('//meta[@name="description"]/@content').get(),
+			'multiple_h1': len(response.css('h1').getall()),
+			'h1_1': response.css('h1::text').getall(),
+			'multiple_canonical': len(response.xpath('//link[@rel="canonical"]/@href').getall()),
+			'canonical_link_1': response.xpath('//link[@rel="canonical"]/@href').get(),
+		}
