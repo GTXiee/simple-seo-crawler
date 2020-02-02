22836bd89dbed85ec83475531c7a0fbeeef7cfe5
George <georgethomaschristie@gmail.com>
2020-02-02 15:04:59 +0000

    Updated before GUI

--- basic_seo_crawler/basic_seo_crawler/spiders/seo_spider.py
+++ basic_seo_crawler/basic_seo_crawler/spiders/seo_spider.py
@@ -5,8 +5,8 @@ from urllib.parse import urlsplit
 
 class SEOSpider(CrawlSpider):
 	name = 'seospider'
-	allowed_domains = ['georgechristie.dev']
-	start_urls = ['https://georgechristie.dev']
+	allowed_domains = ['example.com']
+	start_urls = ['https://example.com']
 
 	rules = (
 		Rule(LinkExtractor(), callback='parse_item'),
@@ -20,8 +20,8 @@ class SEOSpider(CrawlSpider):
 			'status_code': response.status,
 			'title': response.xpath('//title/text()').get(),
 			'meta_description': response.xpath('//meta[@name="description"]/@content').get(),
-			'multiple_h1': len(response.css('h1').getall()),
-			'h1_1': response.css('h1::text').getall(),
+			'multiple_h1': len(response.xpath('//h1').getall()),
+			'h1_1': response.xpath('//h1//text()').get(),
 			'multiple_canonical': len(response.xpath('//link[@rel="canonical"]/@href').getall()),
 			'canonical_link_1': response.xpath('//link[@rel="canonical"]/@href').get(),
 		}
